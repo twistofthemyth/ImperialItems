1ba9960e4788097b36f042aae97d35564218f304
twistofthemyth <twist.of.the.myth@gmail.com>
2022-10-12 22:54:50 +0300

    added some recipes

--- src/main/java/ru/imperiamc/imperialitems/RecipeComponents.java
+++ src/main/java/ru/imperiamc/imperialitems/RecipeComponents.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 public enum RecipeComponents {
     BLOODY_MAP("BloodyMap"),
     BEAST_BLOOD("BeastBlood"),
-    BLOODY_MAP_FRAGMENT("BloodyMapFragment");
+    BLOODY_MAP_FRAGMENT("BloodyMapFragment"),
+    BEWITCHED_SWORD_SHARD("BewitchedSwordShard"),
+    BEWITCHED_AMELIA_SWORD("BewitchedAmeliaSword"),
+    CRYSTAL_SWORD("CrystalSword");
 
     @Getter
     private final String name;
--- src/main/java/ru/imperiamc/imperialitems/ShapedRecipes.java
+++ src/main/java/ru/imperiamc/imperialitems/ShapedRecipes.java
@@ -2,8 +2,6 @@ package ru.imperiamc.imperialitems;
 
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ShapedRecipe;
-import ru.imperiamc.imperialitems.ImperialItems;
-import ru.imperiamc.imperialitems.ItemFileManager;
 
 import static ru.imperiamc.imperialitems.RecipeComponents.*;
 
@@ -15,6 +13,16 @@ public class ShapedRecipes {
     }
 
     public ShapedRecipe bloodyMap() {
+        ItemFileManager componentManager = plugin.getRecipeComponentManager();
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(plugin, BEWITCHED_AMELIA_SWORD.getKey()),
+                componentManager.getOrDefault(BEWITCHED_AMELIA_SWORD.getName()));
+        recipe.shape(" S ", "SCS", " S ");
+        recipe.setIngredient('S', componentManager.getOrDefault(BEWITCHED_SWORD_SHARD.getName()));
+        recipe.setIngredient('C', componentManager.getOrDefault(CRYSTAL_SWORD.getName()));
+        return recipe;
+    }
+
+    public ShapedRecipe bewitchedAmeliaSword() {
         ItemFileManager componentManager = plugin.getRecipeComponentManager();
         ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(plugin, BLOODY_MAP.getKey()),
                 componentManager.getOrDefault(BLOODY_MAP.getName()));
